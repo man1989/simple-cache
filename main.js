0bda982c2c9e6a6bdbc7e5dbe8623a00a6844241
Kumar <kumarma@blr-kumarma1-m.ds.ad.adp.com>
2017-11-19 12:31:05 +0530

    simple cache implementation

---
+++ main.js
@@ -0,0 +1,44 @@
+class Cache {
+    constructor() {
+        this.store = {};
+        this.events = {};
+    }
+    set(key, value, ttl) {
+        this.store[key] = value;
+        if (ttl) {
+            Cache.timerCheck(() => {
+                this.events["expires"] && this.events["expires"](key, value);
+                delete this.store[key];
+            }, ttl);
+        }
+    }
+    get(key) {
+        return this.store[key];
+    }
+    del(key, cb) {
+        let value = undefined;
+        if (this.store[key]) {
+            value = this.store[key];
+            delete this.store[key];
+        }
+        return value;
+    }
+    on(eventType, handler) {
+        this.events[eventType] = handler;
+    }
+    static timerCheck(cb, ttl) {
+        let promise = Promise.resolve(true);
+        return promise.then(() => {
+            let start = Date.now();
+            let end = Date.now() - start;
+            while (end < ttl) {
+                end = Date.now() - start;
+            }
+            if (end <= ttl) {
+                cb();
+            }
+        });
+    }
+}
+
+module.exports = Cache;
---
+++ readme.md
@@ -0,0 +1,20 @@
+#simple-cache
+Caches data locally
+This is a very naive solution, just a showcase how things could be implemented
+
+##usage:-
+```
+const Cache = require("simple-cache");
+let localCache = new Cache();
+
+localCache.set(key, value, [TTL in ms]); //set value
+localCache.set(key, value, 3000); //set value and expires on specified TTL
+localCache.get(key); //get the value
+localCache.del(key); //delete the value, returns deleted value
+localCache.on("expires", (key, value)=>{
+    //when key expires, this event will get called
+});
+```
+
+
+
